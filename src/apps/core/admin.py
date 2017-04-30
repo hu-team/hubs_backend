7162cc971a944fcfb04667efeb1bb2cde4092d1e
Tom Valk <tomvalk@gmail.com>
2017-04-30 17:02:47 +0200

    [TASK] Improve user form.

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from apps.core.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
 	icon = '<i class="material-icons">security</i>'
+	list_display = ('username', 'first_name', 'last_name', 'email', 'is_superuser')
+	list_filter = ('is_superuser',)
+	search_fields = ('username', 'first_name', 'last_name', 'email')
