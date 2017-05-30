114871ba675da35239310211b5f63496f3f8fa5b
Arjan <arjan.shojaeian@gmail.com>
2017-05-30 14:19:22 +0200

    [TASK] Created the view and serializer class for AbsenceReport.

--- src/apps/absence/urls.py
+++ src/apps/absence/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
+from apps.absence import views
+
 router = DefaultRouter()
 
 urlpatterns = [
--- src/apps/absence/views.py
+++ src/apps/absence/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from apps.absence import serializers
+from apps.absence.models import AbsenceReport
+
+
+class AbsenceReportViewSet(viewsets.ModelViewSet):
+	permission_classes = [IsAuthenticated]
+	model = AbsenceReport
+	queryset = AbsenceReport.objects.all()
+	serializer_class = serializers.AbsenceReportSerializer
