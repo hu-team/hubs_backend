3aa3cbf3e812e76eb7333baa57d522b5b76b6b6e
Arjan Shoj <arjan.shojaeian@gmail.com>
2017-06-06 12:27:43 +0200

    Added room field to the LessonSerializer.

--- src/apps/school/serializers.py
+++ src/apps/school/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from apps.core.serializers import StudentSerializer, TeacherSerializer
 from apps.school.models import Lesson, Group, Presence, Course, Result
+import random
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -35,12 +36,19 @@ class LessonSerializer(serializers.ModelSerializer):
 	teacher = TeacherSerializer(read_only=True)
 	course = CourseSerializer(read_only=True)
 	group = GroupSerializer(read_only=True)
+	room = serializers.SerializerMethodField()
+
+	def get_room(self, obj):
+		rooms = ['2.133', '0.053', '1.862', '3.153', '4.830']
+		building_names = ['PL99', 'PL101', 'DL200', 'DL500']
+
+		return random.choice(building_names) + '-' + random.choice(rooms)
 
 	class Meta:
 		model = Lesson
 		fields = (
 			'id', 'course', 'ignore_absence', 'teacher', 'group',
-			'start', 'end',
+			'start', 'end', 'room'
 		)
 
 
