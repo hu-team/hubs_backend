f9f7e653b1e67bb781636f459a83e418e9c053b1
Tom Valk <tomvalk@gmail.com>
2017-06-12 13:00:28 +0200

    [TASK] Fix the post of report.

--- src/apps/absence/serializers.py
+++ src/apps/absence/serializers.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework import serializers
 
 from apps.absence.models import AbsenceReport
@@ -6,7 +7,11 @@ from apps.core.serializers import StudentSerializer
 
 class AbsenceReportSerializer(serializers.ModelSerializer):
 	student = StudentSerializer(read_only=True)
+	student_id = serializers.IntegerField(write_only=True, required=True)
+	report_from = serializers.DateTimeField(default=datetime.datetime.now(), required=False)
+	type = serializers.ChoiceField(choices=AbsenceReport.TYPE_CHOICES, required=True)
+	reason = serializers.CharField(max_length=1000, required=False, default=None)
 
 	class Meta:
 		model = AbsenceReport
-		fields = ('id', 'student', 'report_from', 'report_until', 'type', 'reason')
+		fields = ('id', 'student', 'student_id', 'report_from', 'report_until', 'type', 'reason')
