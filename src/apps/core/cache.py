d4680c29b9b7f8ae2d5f055928632170fd18f4f3
Tom Valk <tomvalk@gmail.com>
2017-06-15 13:47:06 +0200

    [BUGFIX] Fix cache key bit for per user cache.

--- src/apps/core/cache.py
+++ src/apps/core/cache.py
@@ -4,9 +4,21 @@ from rest_framework_extensions.key_constructor import bits
 
 class HubsKeyConstructor(KeyConstructor):
 	unique_method_id = bits.UniqueMethodIdKeyBit()
+	unique_view_id = bits.UniqueViewIdKeyBit()
 	format = bits.FormatKeyBit()
 	language = bits.LanguageKeyBit()
 	query_params = bits.QueryParamsKeyBit()
-	sql_query = bits.SqlQueryKeyBitBase
+	pagination = bits.PaginationKeyBit()
 
-cache_key_func = HubsKeyConstructor()
+
+class HubsPerUserKeyConstructor(KeyConstructor):
+	unique_method_id = bits.UniqueMethodIdKeyBit()
+	unique_view_id = bits.UniqueViewIdKeyBit()
+	format = bits.FormatKeyBit()
+	language = bits.LanguageKeyBit()
+	query_params = bits.QueryParamsKeyBit()
+	pagination = bits.PaginationKeyBit()
+	user = bits.UserKeyBit()
+
+global_cache_key_func = HubsKeyConstructor()
+cache_key_func = HubsPerUserKeyConstructor()
