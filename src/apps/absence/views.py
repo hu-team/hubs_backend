e260503af50879b0b4f5c0e9acb00a7e78feb3e6
Arjan Shoj <arjan.shojaeian@gmail.com>
2017-06-15 14:00:07 +0200

    Implemented the permissions in the viewsets.

--- src/apps/absence/views.py
+++ src/apps/absence/views.py
@@ -3,10 +3,11 @@ from rest_framework.permissions import IsAuthenticated
 
 from apps.absence import serializers
 from apps.absence.models import AbsenceReport
+from apps.core.permissions import StudentReadOnlyAndPost
 
 
 class AbsenceReportViewSet(viewsets.ModelViewSet):
-	permission_classes = [IsAuthenticated]
+	permission_classes = [StudentReadOnlyAndPost]
 	model = AbsenceReport
 	queryset = AbsenceReport.objects.all()
 	serializer_class = serializers.AbsenceReportSerializer
--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail, BadHeaderError
 from rest_framework.response import Response
 
 from apps.core.models import User
+from apps.core.permissions import ReadOnlyOrWriteAccess, StudentNotAllowed
 from apps.core.serializers import MailSerializer
 from apps.school.models import Student, Teacher
 from . import serializers
@@ -17,20 +18,22 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class StudentViewSet(viewsets.ReadOnlyModelViewSet):
-	permission_classes = [IsAuthenticated]
+	permission_classes = [ReadOnlyOrWriteAccess]
 	model = Student
 	queryset = Student.objects.all().select_related('user')
 	serializer_class = serializers.StudentSerializerWithCounselor
 
 
 class TeacherViewSet(viewsets.ReadOnlyModelViewSet):
-	permission_classes = [IsAuthenticated]
+	permission_classes = [ReadOnlyOrWriteAccess]
 	model = Teacher
 	queryset = Teacher.objects.all().select_related('user')
 	serializer_class = serializers.TeacherSerializer
 
 
 class EmailView(views.APIView):
+	permission_classes = [StudentNotAllowed]
+
 	def post(self, request):
 		serializer = MailSerializer(data=request.data)
 		if serializer.is_valid():
