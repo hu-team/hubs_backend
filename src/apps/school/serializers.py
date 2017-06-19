6f1a09143bf200a1cf56896d2a4704a97cc183c6
Tom Valk <tomvalk@gmail.com>
2017-06-19 12:03:20 +0200

    [BUGFIX] Fixing issue in serializer

--- src/apps/school/serializers.py
+++ src/apps/school/serializers.py
@@ -84,7 +84,7 @@ class ResultSerializer(serializers.ModelSerializer):
 	course = CourseSerializer(read_only=True)
 	student = StudentSerializer(read_only=True)
 	number_grade = serializers.IntegerField(required=False)
-	ladder_grade = serializers.DecimalField(required=False, max_digits=2)
+	ladder_grade = serializers.DecimalField(required=False, max_digits=2, decimal_places=1)
 
 	class Meta:
 		model = Result
