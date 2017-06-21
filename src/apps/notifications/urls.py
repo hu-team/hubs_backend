bebb753617c8db16bdd5139c53864a6c432fdba8
Arjan Shoj <arjan.shojaeian@gmail.com>
2017-06-21 12:28:34 +0200

    Added a minified notifications serializer with a viewset and linked it on an URL.

--- src/apps/notifications/urls.py
+++ src/apps/notifications/urls.py
@@ -5,6 +5,7 @@ from apps.notifications import views
 
 router = DefaultRouter()
 router.register(r'notifications', views.NotificationViewSet, base_name='notifications')
+router.register(r'mininotifications', views.NotificationMiniViewSet)
 
 urlpatterns = [
 	url(r'', include(router.urls)),
